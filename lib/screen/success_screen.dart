081405568beaf08790853b45d8ec60b0d37199e4
“hiroshu-diary” <hiroshu.diary@gmail.com>
2022-05-28 16:28:02 +0900

    スタッフ用リセット機能削除

--- lib/screen/success_screen.dart
+++ lib/screen/success_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:seisouchu_alarm/screen/reset_screen.dart';
 
-import '../helper/navi.dart';
 import '../helper/sound.dart';
 
 class SuccessScreen extends StatefulWidget {
@@ -24,13 +22,6 @@ class _SuccessScreenState extends State<SuccessScreen> {
       backgroundColor: Colors.black,
       body: Center(
         child: Image.asset('assets/ss.png', width: 640),
-      ),
-      floatingActionButton: TextButton(
-        child: const Text('   \n　　\n　　'),
-        onPressed: () {},
-        onLongPress: () {
-          Navi.fadeNavi(context, const ResetScreen());
-        },
       ),
     );
   }
